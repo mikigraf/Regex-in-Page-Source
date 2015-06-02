77fac11342f4d10e697312e047f40ede9f385896
Mike Mikolaj  Wawrzyniak <mikolajspejs@gmail.com>
2015-06-02 22:48:30 +0200

    changes
    *added checkboxes for modifiers - didn’t implement the script behind
    them
    
    *added clear and lastResults buttons, without implementing the script
    behind them
    
    *save regex to localStorage and retrive it automatically
    
    *added listeners for new options

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,36 @@
 var result;
+var message = document.querySelector('#message');
+
 chrome.extension.onMessage.addListener(function(request, sender) {
-        if(request.action == "getSource"){
-            var source = request.source;
-            var regex = new RegExp(document.getElementById("regex").value,"g");
-            var matchArray = source.match(regex,"g");
-            message.innerText = result = matchArray.join("\n \n");
-        }
+    var source = request.source;
+    var regex = new RegExp(document.getElementById("regex").value,"g");
+    var matchArray = source.match(regex,"g");
+    localStorage.setItem('regex', document.getElementById("regex").value);
+
+    if(request.action == "getSource"){
+        localStorage.removeItem("results");
+        message.innerText = result = matchArray.join("\n \n");
+        localStorage.setItem("results", result);
+    }
+    if(request.action == "append"){
+        localStorage.removeItem("results");
+        result = message.innerText + (matchArray.join("\n \n"));
+        localStorage.setItem("results", result);
+    }
+    if(request.action == "returnLast"){
+        message.innerText = "last Item";
+    }
+    if(request.action == "clear"){
+        message.innerText = "CLEARED";
+        localStorage.removeItem("results");
+    }
+
 });
 
 function ifPageLoaded(){
     var regexInput = document.getElementById("regex").value;
     var regex = new RegExp(regexInput);
-    var message = document.querySelector('#message');
+   // var message = document.querySelector('#message');
     chrome.tabs.executeScript(null, {
         file: "/js/miner.js"
     }, function(){
@@ -21,4 +40,42 @@ function ifPageLoaded(){
     });
 };
 
+function append(){
+    var regexInput = document.getElementById("regex").value;
+    var regex = new RegExp(regexInput);
+    //var message = document.querySelector('#message');
+    chrome.tabs.executeScript(null, {
+        file: "/js/append.js"
+    }, function(){
+        if(chrome.extension.lastError){
+            message.innerText = "Error \n" + chrome.extension.lastError.message;
+        }
+    });
+}
+
+function lastResults(){
+    chrom.tabs.executeScript(null, {
+        file: "/js/lastResults.js"}, function(){
+        if(chrome.extension.lastError) {
+            message.innerText = "Error \n" + chrome.extension.lastError.message;
+        }
+    })
+
+}
+
+function clearField(){
+    chrom.tabs.executeScript(null, {
+        file: "/js/clear.js"}, function(){
+        if(chrome.extension.lastError) {
+            message.innerText = "Error \n" + chrome.extension.lastError.message;
+        }
+    })
+}
+
+document.getElementById('regex').value = localStorage.getItem('regex');
 document.getElementById('execute').addEventListener('click', ifPageLoaded);
+document.getElementById('append').addEventListener('click', append);
+document.getElementById('last').addEventListener('click', lastResults);
+document.getElementById('clear').addEventListener('click', clearField);
+
+
